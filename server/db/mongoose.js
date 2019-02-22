47ddf6fcd549a218798c6b5dd78925a8dd238631
andanfe@gmail.com <andanfe@gmail.com>
2019-02-22 20:03:07 +0200

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 
+const remoteUri = 'mongodb+srv://andrew:dynamo_1927@todo-app-db-pnknj.mongodb.net/test?retryWrites=true';
+const localUri = 'mongodb://localhost:27017/TodoApp';
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.PORT ? remoteUri: localUri);
 
 
 module.exports = {
