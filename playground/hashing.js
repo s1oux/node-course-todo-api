2ce34722ffca94e06bdc6c0572e4b2ae563ace9d
andanfe@gmail.com <andanfe@gmail.com>
2019-02-23 19:54:09 +0200

    add hashing user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 13
-};
+var password = 'somespassword';
 
-var token = jwt.sign(data, 'key');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, 'key');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$PRlUx.xaU02sRE6BaMKUvuHfr89RGMHLuLHIDytrGTovjtCIK4VCi';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 13
+// };
+//
+// var token = jwt.sign(data, 'key');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, 'key');
+// console.log('decoded', decoded);
+
+
 // var message = 'imma user numbrah 3';
 // var hash = SHA256(message).toString();
 // console.log(`message: ${message}`);
